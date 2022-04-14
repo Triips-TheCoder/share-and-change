23d96499db2425ac8fd0e792ff3c9d9860759a74
Arnaud Levy <contact@arnaudlevy.com>
2022-04-14 14:56:38 +0200

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,13 @@ alimentation = Need.where(name: 'Alimentation').first_or_create
 emploi = Need.where(name: 'Emploi').first_or_create
 formation = Need.where(name: 'Formation').first_or_create
 logement = Need.where(name: 'Logement').first_or_create
-adminstration = Need.where(name: 'Admnistration').first_or_create
+administration = Need.where(name: 'Administration').first_or_create
 sante = Need.where(name: 'Santé').first_or_create
 
 #Charities
-
 secours_pop = Charity.where(name: 'Secours Populaire').first_or_create
 secours_pop.description = "Le Secours Populaire est une association reconnue d’utilité publique “agir pour un monde plus juste et plus solidaire”. L’association permet ainsi de s’émanciper et trouver sa place de citoyen. Issu du peuple, animé par lui, le Secours populaire promeut une relation d’égal à égal véritablement unique et un accueil inconditionnel. Présent partout, au bout de la rue comme au bout du monde avec son réseau de partenaires, il valorise systématiquement l’initiative comme mode d’action. L’association est profondément décentralisée et chacun peut ainsi agir : actions d’urgence, actions dans la durée, accompagnement global, sur les plans matériel, alimentaire, médical, moral, juridique, de la formation, de l’insertion sociale et professionnelle, ainsi que de l’accès au droit, à la culture et aux loisirs, au numérique, etc. Bénévoles comme partenaires, entreprises et donateurs, adultes comme enfants dès le plus jeune âge… tout le monde est invité à passer à l’action, à s'émanciper, à vivre la solidarité jour après jour. Ancrant sa vision sur la volonté d’un monde plus solidaire, le Secours populaire s’abstient de tout clivage : il rassemble et crée du lien autour des valeurs partagées."
 secours_pop.save
-
 
 singa = Charity.where(name: 'Singa').first_or_create
 singa.logo = "/assets/png/Singa.png"
@@ -36,12 +34,11 @@ singa.zipcode = "33100"
 singa.save
 
 #Capsule
-
-capsule = Capsule.where(charity: secours_pop, need: alimentation).first_or_create
+capsule = Capsule.where(charity: secours_pop, need: emploi).first_or_create
 capsule.first_name = 'Alaa'
 capsule.video = '699029249'
 capsule.poster = 'https://i.vimeocdn.com/video/1413371886-7bcc569ada7f4a9049463716c6915187139139d4d41a431d2923a5a43c33f932-d'
-capsule.quote = "Il faut aller travailler avec l'associtaion Singa pour faire grandir son projet"
+capsule.quote = "Il faut aller travailler avec l'association Singa pour faire grandir son projet"
 capsule.save
 
 capsule = Capsule.where(charity: secours_pop, need: alimentation).first_or_create
