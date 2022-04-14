377fe2ea82192334bca68e9f5f53344538018f35
Julian Laballe <julian.laballe@mmibordeaux.com>
2022-04-14 15:26:01 +0200

    correct menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,18 @@
 const rootElement = document.documentElement
 const menuButton = document.querySelector('#menu-button')
 
-rootElement.addEventListener('click', openNavigation)
+menuButton.addEventListener('click', openNavigation)
+let isOpen = false 
 
 function openNavigation() {
-    rootElement.classList.toggle('navigation-open')
+    if (!isOpen)
+        rootElement.classList.add('navigation-open')
+    else
+        rootElement.classList.remove('navigation-open')
+
+    toggleOpen()
+}
+
+function toggleOpen() {
+    return isOpen = !isOpen
 }
