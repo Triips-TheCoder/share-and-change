00c27bc9529e12b619a992aaa3180e5af1a3be82
FranciaMMI <francia.bidzimou@mmibordeaux.com>
2023-03-31 02:17:23 +0200

    admin interface

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
   def index_admin
+      @capsules = Capsule.all.sample 6
+      @charities = Charity.all.sample 5
+      @needs = Need.all
   end
 
   def qui_sommes_nous
